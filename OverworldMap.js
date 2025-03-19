b61923c411b6f74c3cf2cc6ace0bc18097d9e6bc
Guilherme Campos <guilherme.campos@tecnico.ulisboa.pt>
2025-03-19 14:36:29 +0000

    new coordinate for talking to operator

--- OverworldMap.js
+++ OverworldMap.js
@@ -513,6 +513,7 @@ window.OverworldMaps = {
             // Talking to operator event (35.5, 19)
             [utils.asGridCoords(32.5, 18)]: introLevel1Event,
             [utils.asGridCoords(31.5, 19)]: introLevel1Event,
+            [utils.asGridCoords(33.5, 17)]: introLevel1Event,
 
             // Button spaces for the debris items
             // Debris 1: (35.5, 24)
